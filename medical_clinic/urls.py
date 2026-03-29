7a27dd8e8622db38b44746248c7da91f6952e578
Aryan <nsaryan724@gmail.com>
2026-03-29 14:21:56 +0530

    Add .html routes and connect nav links

--- medical_clinic/urls.py
+++ medical_clinic/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('about-doctor/', views.about_doctor, name='about_doctor'),
     path('services/', views.services, name='services'),
     path('book-appointment/', views.book_appointment, name='book_appointment'),
+
+    path('home.html', views.home, name='home_html'),
+    path('about-doctor.html', views.about_doctor, name='about_doctor_html'),
+    path('about_doctor.html', views.about_doctor, name='about_doctor_html_underscore'),
+    path('services.html', views.services, name='services_html'),
+    path('book-appointment.html', views.book_appointment, name='book_appointment_html'),
+    path('book_appointment.html', views.book_appointment, name='book_appointment_html_underscore'),
 ]
